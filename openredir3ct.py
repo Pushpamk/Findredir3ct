cf83e9e30b409f0200ea9396e7b27bb37cdc7daf
Pushpam Kumar <kumarpushpam99@gmail.com>
2018-05-01 03:36:03 +0530

    01/05/2018

--- openredir3ct.py
+++ openredir3ct.py
@@ -16,7 +16,7 @@ def banner(ip):
 			for line in f.readlines():
 				payload = line.strip('\n')
 				r = requests.post(ip + payload)
-				print "\033[1;34m[?]" + r.url + "\033[1;m"
+				print "\033[1;34m[?]" + r.url + "\033[1;m" + "\033[1;31m[-]No Redirection\033[1;m"
 			if r.status_code == 301 :
 				return true
 	except Exception, e:
@@ -26,7 +26,5 @@ def main():
 	ip = sys.argv[1]
 	if banner(ip):
 		print "\033[1;32m[+] Redirection Successfull" 
-	else: 
-		print "\033[1;31m[-]No Redirection"
 if __name__ == '__main__':
 	main()
